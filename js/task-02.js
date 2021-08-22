48a16f377102c95ad4cb5e86375b90cf7be581a5
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-08-22 23:45:15 +0300

    Добавил решение второй задачи

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,12 @@
 const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
+
+const ingredientsEl = document.getElementById('ingredients');
+
+function createMurkup(options) {
+    return options.map(option => {
+        let li = document.createElement('li');
+        li.textContent = option;
+        return li;
+    });
+}
+ingredientsEl.append(...createMurkup(ingredients));
