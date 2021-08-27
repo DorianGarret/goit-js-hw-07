1582b418bdec763a0cf0ff65dbb7fd8767a32cf8
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-08-27 11:49:17 +0300

    Исправил решение седьмой задачи

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('font-size-control');
 const text = document.getElementById('text');
 
-input.addEventListener('input', function (event) {
-    text.style.fontSize = `${event.currentTarget.value}px`;
-});
+input.addEventListener(
+    'input',
+    getSizeText => (text.style.fontSize = `${getSizeText.currentTarget.value}px`),
+);
