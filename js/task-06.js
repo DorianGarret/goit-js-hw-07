b5b7da8be42784b8f9c822ba6277b934af75563d
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-08-27 11:43:01 +0300

    Исправил решение шестой задачи

--- js/task-06.js
+++ js/task-06.js
@@ -5,7 +5,7 @@ input.addEventListener('blur', checkValueInput);
 function checkValueInput(event) {
     const lenghtValue = this.dataset.length;
 
-    event.currentTarget.value.length === +lenghtValue
+    event.currentTarget.value.length === Number(lenghtValue)
         ? (input.classList = 'valid')
         : (input.classList = 'invalid');
 }
